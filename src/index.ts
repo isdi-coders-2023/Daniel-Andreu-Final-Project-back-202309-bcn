56a67f1bc42fdb6662a660923f1f0ae763b3e6a4
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-28 12:04:27 +0100

    Setup src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-// Director de orquesta 🦥
+import "dotenv/config";
+import debugCreator from "debug";
+import chalk from "chalk";
+import { startServer } from "./server/app.js";
+import { connectToDatabase } from "./database/index.js";
+import "./server/index.js";
+
+const debug = debugCreator("shoes:main");
+
+const port = process.env.PORT ?? 4000;
+
+if (!process.env.MONGODB_URL) {
+  debug(chalk.red("Missing MongoDB connection string"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+startServer(+port);
