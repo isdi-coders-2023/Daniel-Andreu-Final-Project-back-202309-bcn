e1ca76a820c182c984893af7908eeb2c2cf79970
Daniel Andreu <daniel1997ar@gmail.com>
2023-12-10 13:19:26 +0100

    Add addShoe method in ShoesController

--- src/features/shoe/controller/ShoesController.ts
+++ src/features/shoe/controller/ShoesController.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import type { ShoesRepositoryStructure } from "../repository/types";
 import CustomError from "../../../CustomError/CustomError.js";
+import { type ShoeCreateRequest } from "../types";
 
 class ShoesController {
   constructor(private readonly shoesRepository: ShoesRepositoryStructure) {}
@@ -23,6 +24,21 @@ class ShoesController {
     } catch {
       const error = new CustomError("Error deleting the shoe", 400);
       next(error);
+    }
+  };
+
+  public addShoe = async (
+    req: ShoeCreateRequest,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const shoeData = req.body;
+    try {
+      const newShoe = await this.shoesRepository.addShoe(shoeData);
+      res.status(201).json({ shoe: newShoe });
+    } catch (error) {
+      const customError = new CustomError("Error creating the new shoe", 500);
+      next(customError);
     }
   };
 }
